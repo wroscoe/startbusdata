6e0b17a3a1c74b33e185023af13862bc623c8fca
wroscoe <wroscoe@gmail.com>
2024-09-27 11:05:37 -0600

    g

---
+++ pages/1_Ridership.py
@@ -0,0 +1,100 @@
+import streamlit as st
+import pandas as pd
+import math
+from pathlib import Path
+
+# Set the title and favicon that appear in the Browser's tab bar.
+st.set_page_config(
+    page_title='GDP dashboard',
+    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+)   
+
+# -----------------------------------------------------------------------------
+# Declare some useful functions.
+
+@st.cache_data
+def get_gdp_data():
+    """Grab GDP data from a CSV file.
+
+    This uses caching to avoid having to read the file every time. If we were
+    reading from an HTTP endpoint instead of a file, it's a good idea to set
+    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
+    """
+
+    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
+    DATA_FILENAME = Path(__file__).parent/'data/start_monthly_ridership_data.csv'
+    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+
+
+    return raw_gdp_df
+
+gdp_df = get_gdp_data()
+
+# -----------------------------------------------------------------------------
+# Draw the actual page
+
+# Set the title that appears at the top of the page.
+'''
+# :earth_americas: GDP dashboard
+
+Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
+notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
+But it's otherwise a great (and did I mention _free_?) source of data.
+'''
+
+# Add some spacing
+''
+''
+
+min_value = gdp_df['Year'].min()
+max_value = gdp_df['Year'].max()
+
+from_year, to_year = st.slider(
+    'Which years are you interested in?',
+    min_value=min_value,
+    max_value=max_value,
+    value=[min_value, max_value])
+
+routes = gdp_df['Route'].unique()
+
+if not len(routes):
+    st.warning("Select at least one country")
+
+selected_routes = st.multiselect(
+    'Which routes would you like to view?',
+    routes,
+    routes)
+
+''
+''
+''
+
+# Filter the data
+filtered_gdp_df = gdp_df[
+    (gdp_df['Route'].isin(selected_routes))
+    & (gdp_df['Year'] <= to_year)
+    & (from_year <= gdp_df['Year'])
+]
+
+st.header('GDP over time', divider='gray')
+
+''
+
+st.bar_chart(
+    filtered_gdp_df,
+    x='Year',
+    y='Riders',
+    color='Route',
+    stack=False,
+)
+
+''
+''
+
+
+first_year = gdp_df[gdp_df['Year'] == from_year]
+last_year = gdp_df[gdp_df['Year'] == to_year]
+
+st.header(f'GDP in {to_year}', divider='gray')
+
+''

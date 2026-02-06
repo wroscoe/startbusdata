a6f9b48e65134ddaa7197a1bfd69f0ccc2fe41d1
wroscoe <wroscoe@gmail.com>
2026-02-06 11:17:05 -0700

    Update On Demand page

--- pages/3_On_Demand.py
+++ pages/3_On_Demand.py
@@ -23,9 +23,6 @@ of on-demand rides as two different color heatmaps. The locations are randomized
 pickup_hours = st.sidebar.multiselect('Select Pickup Hour(s) (select between 0 and 23)', options=list(range(24)), default=[6, 7, 8])
 filtered_data = data[data['pickup_hour'].isin(pickup_hours)]
 
-# Toggle options in sidebar to display/hide heatmaps
-show_pickup_heatmap = st.sidebar.checkbox('Show Pickup Heatmap', value=True)
-show_dropoff_heatmap = st.sidebar.checkbox('Show Dropoff Heatmap', value=True)
 
 # Define map center (average of pickup locations)
 center_lat = filtered_data['pickup_lat'].mean()
@@ -35,14 +32,12 @@ center_lon = filtered_data['pickup_lon'].mean()
 map_ = folium.Map(location=[center_lat, center_lon], zoom_start=13)
 
 # Add pickup heatmap layer if selected
-if show_pickup_heatmap:
-    pickup_points = filtered_data[['pickup_lat', 'pickup_lon']].values.tolist()
-    HeatMap(pickup_points, name='Pickups', gradient={0.4: 'blue', 0.65: 'cyan', 1: 'lime'}).add_to(map_)
+pickup_points = filtered_data[['pickup_lat', 'pickup_lon']].values.tolist()
+HeatMap(pickup_points, name='Pickups', gradient={0.4: 'blue', 0.65: 'cyan', 1: 'lime'}).add_to(map_)
 
 # Add dropoff heatmap layer if selected
-if show_dropoff_heatmap:
-    dropoff_points = filtered_data[['dropoff_lat', 'dropoff_lon']].values.tolist()
-    HeatMap(dropoff_points, name='Dropoffs', gradient={0.4: 'red', 0.65: 'orange', 1: 'yellow'}).add_to(map_)
+dropoff_points = filtered_data[['dropoff_lat', 'dropoff_lon']].values.tolist()
+HeatMap(dropoff_points, name='Dropoffs', gradient={0.4: 'red', 0.65: 'orange', 1: 'yellow'}).add_to(map_)
 
 # Add layer control
 folium.LayerControl().add_to(map_)
